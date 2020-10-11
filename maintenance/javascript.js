ba5a588aba2b745aa14de4f9cb22685391e2cb07
PenguinLucky <hiroatsu11_maron9_marin7@yahoo.co.jp>
2020-10-11 21:09:05 +0900

    Create ./maintenance/*

---
+++ maintenance/javascript.js
@@ -0,0 +1,59 @@
+jQuery(function() {
+
+    function ErrorPage(container, pageType, templateName) {
+        this.$container = $(container);
+        this.$contentContainer = this.$container.find(templateName == 'sign' ? '.sign-container' : '.content-container');
+        this.pageType = pageType;
+        this.templateName = templateName;
+    }
+
+    ErrorPage.prototype.centerContent = function () {
+        var containerHeight = this.$container.outerHeight() || null
+            , contentContainerHeight = this.$contentContainer.outerHeight() || null
+            , top = (containerHeight - contentContainerHeight) / 2
+            , offset = this.templateName == 'sign' ? -100 : 0;
+
+        this.$contentContainer.css('top', top + offset);
+    };
+
+    ErrorPage.prototype.initialize = function () {
+        var self = this;
+
+        this.centerContent();
+        this.$container.on('resize', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            self.centerContent();
+        });
+
+        // fades in content on the plain template
+        if (this.templateName == 'plain') {
+            window.setTimeout(function () {
+                self.$contentContainer.addClass('in');
+            }, 500);
+        }
+
+        // swings sign in on the sign template
+        if (this.templateName == 'sign') {
+            $('.sign-container').animate({ textIndent: 0 }, {
+                step: function (now) {
+                    $(this).css({
+                        transform: 'rotate(' + now + 'deg)',
+                        'transform-origin': 'top center'
+                    });
+                },
+                duration: 1000,
+                easing: 'easeOutBounce'
+            });
+        }
+    };
+
+    var ep = new ErrorPage('body', "maintenance", "sign");
+    ep.initialize();
+
+    // hack to make sure content stays centered >_<
+    $(window).on('resize', function () {
+        $('body').trigger('resize')
+    });
+
+});
